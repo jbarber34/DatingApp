dfe1bfe7b082aaec700144e785c8faba17dd1668
James Barber <jamesbarber@twinsbaseball.com>
2022-12-31 15:57:48 -0500

    Handle validation errors

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -26,11 +26,11 @@ export class ErrorInterceptor implements HttpInterceptor {
               if (error.error.errors) {
                 const modelStateErrors = [];
                 for (const key in error.error.errors) {
-                  if (error.error.erors[key]) {
+                  if (error.error.errors[key]) {
                     modelStateErrors.push(error.error.errors[key]);
                   }
                 }
-                throw modelStateErrors;
+                throw modelStateErrors.flat();
               } else {
                 this.toastr.error(error.error, error.status.toString());
               }
