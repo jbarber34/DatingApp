bbe1329e9cf968695795cd383002264976670e30
James Barber <jamesbarber@twinsbaseball.com>
2023-01-24 21:05:46 -0500

    Create initial files for a reusable text input

---
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css'],
+})
+export class TextInputComponent implements ControlValueAccessor {
+  constructor(@Self() public ngControl: NgControl) {}
+
+  writeValue(obj: any): void {}
+
+  registerOnChange(fn: any): void {}
+
+  registerOnTouched(fn: any): void {}
+}
