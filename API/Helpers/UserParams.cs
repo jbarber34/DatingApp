9681e90a0ae361f7aa5383a13283bee119598f7a
James Barber <jpbarber34@gmail.com>
2023-02-04 17:49:35 -0500

    Small type fix

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -8,7 +8,7 @@ namespace API.Helpers
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
         
     }
