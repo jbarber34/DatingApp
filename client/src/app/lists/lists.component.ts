5a41226692b76795f7137aad9e7569ec66e55480
James Barber <jpbarber34@gmail.com>
2023-02-18 16:52:20 -0500

    Add a 'Likes' component

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MemberService } from '../_services/member.service';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent implements OnInit {
+  members: Member[] | undefined;
+  predicate = 'liked';
 
-  constructor() { }
+  constructor(private memberService: MemberService) {}
 
   ngOnInit(): void {
+    this.loadLikes();
   }
 
+  loadLikes() {
+    this.memberService.getLikes(this.predicate).subscribe({
+      next: (response) => {
+        this.members = response;
+      },
+    });
+  }
 }
