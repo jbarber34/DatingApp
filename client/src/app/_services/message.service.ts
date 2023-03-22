9a7b3fcc7a885f754f34077364661474bc47ea3c
James Barber <jamesbarber@twinsbaseball.com>
2023-03-22 06:36:02 -0400

    Add ability to send messages with the hub

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
@@ -32,6 +32,14 @@ export class MessageService {
     this.hubConnection.on('ReceiveMessageThread', (messages) => {
       this.messageThreadSource.next(messages);
     });
+
+    this.hubConnection.on('NewMessage', (message) => {
+      this.messageThread$.pipe(take(1)).subscribe({
+        next: (messages) => {
+          this.messageThreadSource.next([...messages, message]);
+        },
+      });
+    });
   }
 
   stopHubConnection() {
@@ -56,11 +64,13 @@ export class MessageService {
     );
   }
 
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + 'messages', {
-      recipientUserName: username,
-      content,
-    });
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection
+      ?.invoke('SendMessage', {
+        recipientUserName: username,
+        content,
+      })
+      .catch((error) => console.log(error));
   }
 
   deleteMessage(id: number) {
