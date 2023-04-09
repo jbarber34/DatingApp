f2551bc41efba6f82d72ad65698077be4bbc5908
James Barber <jamesbarber@twinsbaseball.com>
2023-04-09 09:38:12 -0400

    Add new approve/reject methods to admin service as well as to the photo-management.component file

--- client/src/app/_services/admin.service.ts
+++ client/src/app/_services/admin.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
+import { Photo } from '../_models/photo';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,21 @@ export class AdminService {
   baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
+
+  approvePhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/approve-photo/' + photoId, {});
+  }
+
+  getPhotosForApproval() {
+    return this.http.get<Photo[]>(this.baseUrl + 'admin/photos-to-moderate');
+  }
+
   getUsersWithRoles() {
     return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
+  }
+
+  rejectPhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/reject-photo/' + photoId, {});
   }
 
   updateUserRoles(username: string, roles: string[]) {
