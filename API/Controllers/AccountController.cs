ca0610d9a0bbf977e54b0b62ed8c8b3848aed28a
James Barber <jamesbarber@twinsbaseball.com>
2023-10-12 14:16:24 -0400

    Add new roles to the JWT token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,10 +37,14 @@ namespace API.Controllers
 
             if (!result.Succeeded) return BadRequest(result.Errors);
 
+            var rolesResult = await _userManager.AddToRoleAsync(user, "Member");
+
+            if (!rolesResult.Succeeded) return BadRequest(result.Errors);
+
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender,
             };
@@ -62,7 +66,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
