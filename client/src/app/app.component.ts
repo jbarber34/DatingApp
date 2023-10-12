d3fa41dc998b5f94037200f51b4fe2e625422a06
James Barber <jamesbarber@twinsbaseball.com>
2023-10-12 14:16:20 -0400

    Parent to child and child to parent communication; hooking up register method to the service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -10,23 +10,15 @@ import { AccountService } from './_services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'Dating App'; // Don't need to specify type because a string is hard coded
-  users: any;
+  // users: any;
 
   constructor(
-    private http: HttpClient,
+    // private http: HttpClient,
     private accountService: AccountService
   ) {}
   ngOnInit(): void {
-    this.getUsers();
+    // this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
   }
 
   setCurrentUser() {
